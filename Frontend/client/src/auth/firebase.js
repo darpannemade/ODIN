d22c1dfe9a01eb05fa65dd989799ec8ac5372e6f
darpannemade <73686650+darpannemade@users.noreply.github.com>
2025-10-04 22:00:10 +0530

    firebase.js

--- Frontend/client/src/auth/firebase.js
+++ Frontend/client/src/auth/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBf_jhiSGPwzp-wptRw91gpPIKyJApog3c",
-  authDomain: "odin-auth-e9f0e.firebaseapp.com",
-  projectId: "odin-auth-e9f0e",
-  storageBucket: "odin-auth-e9f0e.appspot.com",
-  messagingSenderId: "598155331993",
-  appId: "1:598155331993:web:5c61cbcb24baaa2af52b08",
+  apiKey: "Your_apiKey",
+  authDomain: "Your_authDomain",
+  projectId: "Your_projectId",
+  storageBucket: "Your_storageBucket",
+  messagingSenderId: "Your_messagingSenderId",
+  appId: "Your_appId",
 };
 
 // Initialize Firebase
